3fe02605b96681fa80c7c6f68de98a33f201c113
JayabrataBasu2 <133545838+JayabrataBasu@users.noreply.github.com>
2025-02-08 19:07:19 +0530

    Life is a journey, lets try picking up the Fragments

---
+++ app/src/main/java/com/example/axiom/FourthFragment.kt
@@ -0,0 +1,36 @@
+package com.example.axiom
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.axiom.databinding.FragmentFourthBinding
+
+class FourthFragment : Fragment() {
+
+    private var _binding: FragmentFourthBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFourthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFourth.setOnClickListener {
+            findNavController().navigate(R.id.action_FourthFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+} 
